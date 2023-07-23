51d7cd08cca36c4bd4ab6ab4c95825045c87e6f2
Dafa Zakhulhaq <dafa27890@gmail.com>
2023-07-24 06:30:04 +0700

    refactor : delete dummy api response

--- src/api/categories/model.ts
+++ src/api/categories/model.ts
@@ -13,36 +13,3 @@ export type GetCategories = {
 export type GetDetailCategory = {
   data: Category
 } & MainResponse
-
-// TODO : Remove it when api ready
-export const dummyCategoriesListResponse: GetCategories = {
-  status: true,
-  message: 'Success',
-  data: [
-    {
-      _id: '1',
-      icon: 'https://robohash.org/autquiaut.png?size=50x50&set=set1',
-      name: 'Robot',
-    },
-    {
-      _id: '2',
-      icon: 'https://robohash.org/autquiaut.png?size=50x50&set=set1',
-      name: 'Robit',
-    },
-  ],
-  page: 1,
-  limit: 10,
-  total_data: 100,
-  total_page: 10,
-}
-
-// TODO : Remove it when api ready
-export const dummyCategoryResponse: GetDetailCategory = {
-  status: true,
-  message: 'Success',
-  data: {
-    _id: '1',
-    icon: 'https://robohash.org/autquiaut.png?size=50x50&set=set1',
-    name: 'Robot',
-  },
-}
