eb9c48c8a6ea53facaec3eb1eff022245f61c43d
Dafa Zakhulhaq <dafa27890@gmail.com>
2023-09-04 22:22:32 +0700

    refactor: remove some modules

--- src/app/(dashboard)/notifications/create/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import Form from '../_components/Form'
-
-export default function page() {
-  return <Form />
-}
--- src/components/layouts/menu.ts
+++ src/components/layouts/menu.ts
@@ -1,4 +1,4 @@
-import { HiBell, HiDatabase, HiTag, HiUser } from 'react-icons/hi'
+import { HiUser } from 'react-icons/hi'
 import { SidebarItemProps } from './types'
 
 const menu: SidebarItemProps[] = [
@@ -6,24 +6,6 @@ const menu: SidebarItemProps[] = [
     icon: HiUser,
     title: 'User',
     link: '/user',
-    access: ['Admin', 'Master'],
-  },
-  {
-    icon: HiDatabase,
-    title: 'Categories',
-    link: '/categories',
-    access: ['Admin', 'Master'],
-  },
-  {
-    icon: HiTag,
-    title: 'Products',
-    link: '/products',
-    access: ['Admin', 'Master'],
-  },
-  {
-    icon: HiBell,
-    title: 'Notifications',
-    link: '/notifications',
     access: ['Admin', 'Master'],
   },
 ]
