9db5700da9686f98d9e162b4c30adabaa14f6d46
dafa zakhulhaq <dafa27890@gmail.com>
2024-12-13 07:20:26 +0700

    feat: integrate dashboard API

--- src/api/dashboard/index.ts
+++ src/api/dashboard/index.ts
@@ -1,14 +1,8 @@
+import { fetcher } from '@/utils/fetcher'
+import { GetAbsencePercentage, GetGraphAbsence, GetReportCard } from './model'
+
 export const getReportCard = async () =>
-  //   fetcher<GetReportCard>({ path: '/v1/dashboard/report-card' })
-  ({
-    status: true,
-    message: 'success',
-    data: {
-      p_absen: 0,
-      t_personil: 100,
-      t_project: 100,
-    },
-  })
+  fetcher<GetReportCard>({ path: '/v1/dashboard/report-card' })
 
 type GraphAbsenceParams = {
   project_id?: string
@@ -16,40 +10,13 @@ type GraphAbsenceParams = {
   user_id?: string
 }
 
-/*eslint no-unused-vars: "off"*/
 export const getGraphAbsence = async (params: GraphAbsenceParams) =>
-  //   fetcher<GetGraphAbsence>({
-  //     path: '/v1/dashboard/grafik-absen',
-  //     params: params,
-  //   })
-  ({
-    status: true,
-    message: 'Sukses',
-    data: [
-      'jan',
-      'feb',
-      'mar',
-      'apr',
-      'may',
-      'jun',
-      'jul',
-      'aug',
-      'sep',
-      'oct',
-      'nov',
-      'dec',
-    ],
-    value: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 12],
+  fetcher<GetGraphAbsence>({
+    path: '/v1/dashboard/grafik-absen',
+    params: params,
   })
 
 export const getAbsencePercentage = async () =>
-  //   fetcher<GetReportCard>({ path: '/v1/dashboard/persentasi_absen' })
-  ({
-    Status: true,
-    message: 'Sukese',
-    data: {
-      masuk: 50,
-      sakit: 2,
-      izin: 4,
-    },
+  fetcher<GetAbsencePercentage>({
+    path: '/v1/dashboard/grafik-persentasi-absen',
   })
